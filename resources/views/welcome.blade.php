b5ea972bbcae76341c4cfc0129d3596613467214
kolejuniti <kolejuniti.lms@uniti.edu.my>
2022-06-10 09:50:58 +0800

    update welcome file 9:50

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    My Laravel 9:47
+                    My Laravel 9:50
                 </div>
 
                 <div class="links">
